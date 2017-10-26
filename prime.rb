c3e094cfad6e30a93fb1e9db9b24072219d99559
Eric McKinney <mckinney99@gmail.com>
2017-10-26 15:50:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
-# Add  code here!
-def prime(i)
-  if i == prime.i
-    return true
-  else
-    return false
 
-end
+
+def prime(n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
